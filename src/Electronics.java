2b8b5b87b5eaea8d46bbcaca17271bae794374d0
Rumeasiyan <srumeasiyan@gmail.com>
2024-01-11 20:30:18 +0530

    adds electronics class

---
+++ src/Electronics.java
@@ -0,0 +1,26 @@
+class Electronics extends Product {
+    private String brand;
+    private int warrantyPeriod;
+
+    public Electronics(String productId, String productName, int noOfAvailableItems, double price, String productType, String brand, int warrantyPeriod) {
+        super(productId, productName, noOfAvailableItems, price, "Electronics");
+        this.brand = brand;
+        this.warrantyPeriod = warrantyPeriod;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getWarrantyPeriod() {
+        return warrantyPeriod;
+    }
+
+    public void setWarrantyPeriod(int warrantyPeriod) {
+        this.warrantyPeriod = warrantyPeriod;
+    }
+}
