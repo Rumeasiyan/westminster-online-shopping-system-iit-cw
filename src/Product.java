9256a8ee04829e06269f011f512144f89980a4df
Rumeasiyan <srumeasiyan@gmail.com>
2024-01-11 20:24:16 +0530

    adds product class

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,55 @@
 abstract class Product {
+    private String productId;
+    private String productName;
+    private int noOfAvailableItems;
+    private double price;
+    private String productType;
+
+    public Product(String productId, String productName, int noOfAvailableItems, double price, String productType) {
+        this.productId = productId;
+        this.productName = productName;
+        this.noOfAvailableItems = noOfAvailableItems;
+        this.price = price;
+        this.productType = productType;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getNoOfAvailableItems() {
+        return noOfAvailableItems;
+    }
+
+    public void setNoOfAvailableItems(int noOfAvailableItems) {
+        this.noOfAvailableItems = noOfAvailableItems;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
 }
